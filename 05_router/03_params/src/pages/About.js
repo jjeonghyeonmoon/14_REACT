c7ed14eb11d0935941df49172b2133886233af64
answjdgus72 <answjdgus72@gmail.com>
2025-02-05 18:22:16 +0900

    no message

---
+++ 05_router/03_params/src/pages/About.js
@@ -0,0 +1,13 @@
+/* /about 요청이 들어오면 보여 줄 페이지 */ 
+
+function About() {
+    return(
+        <div>
+            <h1>하이미디어 소개 메뉴리스트!! </h1>
+        </div>
+    );
+};
+
+export default About;
+
+// npm install react@18 react-dom@18 react-router-dom@6
